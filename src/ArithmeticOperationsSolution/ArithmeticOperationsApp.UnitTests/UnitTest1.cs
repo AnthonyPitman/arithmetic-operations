279bff81994041a950cc12c215e65c05dfc47de4
Anthony Pitman <42813672+AnthonyPitman@users.noreply.github.com>
2025-06-05 12:56:51 -0400

    initialize testing project

---
+++ src/ArithmeticOperationsSolution/ArithmeticOperationsApp.UnitTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace ArithmeticOperationsApp.UnitTests;
+
+public class UnitTest1
+{
+    // TODO: add unit testing around Number.Add
+    [Fact]
+    public void Test1()
+    {
+        var result = Number.Add(new Number("1"), new Number("2"));
+        Assert.Equal("3", result.ToString());
+    }
+}
